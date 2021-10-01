d518de0740cd9376211e5d8661205f59c8655934
Tussnsw <91057991+Tussnsw@users.noreply.github.com>
2021-10-01 20:50:49 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/screen/Carlist.dart';
-import 'package:helloworld/screen/LoginScreen.dart';
-import 'package:helloworld/screen/MapScreen.dart';
+import 'package:helloworld/screen/LoginScreen.dart' as global;
+import 'package:helloworld/screen/MapUser.dart';
+import 'package:helloworld/screen/car1.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: global.LoginScreen(),
         debugShowCheckedModeBanner: false,
         routes: {
-          '/login': (context) => LoginScreen(),
-          '/map': (context) => MapsDemo(),
-          '/carlist': (context) => Carlist(),
+          '/login': (context) => global.LoginScreen(),
+          '/map': (context) => MapsDemo(plate: 'none',),
+          '/carlist': (context) => CarDemo(),
+          '/admincar' : (context) => admincar(),
         }
     );
   }
